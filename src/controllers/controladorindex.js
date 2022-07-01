a1e8c1ac2a2179196158a6321b48d2f233ae877d
kevin <kevinvalencia3071029@gmail.com>
2022-07-01 01:58:40 -0500

    se crea interfax index

---
+++ src/controllers/controladorindex.js
@@ -0,0 +1,23 @@
+// entradas == variables
+let nombre = 'Kevin'
+let numero =5
+let numeros = [1,2,3,4,5]
+let politico ={
+    nombre :'alvaro',
+    poder : 'dinero',
+    edad: 64
+}
+
+
+//controlando etiquetas
+// 1. nesecito almacenar una etiqueta en una variable 
+
+let titulo=document.getElementById('titulo')
+let imagen = document.getElementById('foto')
+//2. identifico que quiero controlar
+//2.1 cambio el texto
+
+titulo.textContent='CHAO'
+
+//2.2 cambiando la fuente 
+imagen.src="https://placeimg.com/640/480/any"
