ca18cf52adfbfe87078e871802c32a6565a632e2
Stiven Palacio <stivenchoo11@gmail.com>
2022-07-01 12:19:12 -0500

    clase del 01 de julio

--- src/controllers/controladorindex.js
+++ src/controllers/controladorindex.js
@@ -12,12 +12,32 @@ let politico ={
 //controlando etiquetas
 // 1. nesecito almacenar una etiqueta en una variable 
 
-let titulo=document.getElementById('titulo')
+let titulo = document.getElementById('titulo')
 let imagen = document.getElementById('foto')
+let titulo2 = document.getElementById('titulo2')
+let boton = document.getElementById('boton')
 //2. identifico que quiero controlar
 //2.1 cambio el texto
 
-titulo.textContent='CHAO'
+
 
 //2.2 cambiando la fuente 
-imagen.src="https://placeimg.com/640/480/any"
+
+
+
+//3 Modificando estilos
+//3.1 Agregando una clase
+titulo.classList.add('text-danger','fuente2')
+
+//3.2 Quitando una clase
+titulo2.classList.remove('text-info')
+titulo2.classList.add('text-danger')
+
+//4 escuchando eventos
+boton.addEventListener('click',function(){
+    titulo.textContent='CHAO'
+    imagen.src="https://placeimg.com/640/480/any"
+})
+
+
+
